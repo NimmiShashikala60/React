37fb2592a6a9e605adb640a7b7896bc21bf5bd87
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-03-17 20:46:12 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+// class based
+  componentDidMount():void{
+    console.log("Hii");
+  }
+  render():React.ReactNode{
+    return (
+      <div className="App">
+        <h1>Shashi App</h1>
+      </div>
+    );
+  }
+
+  
 }
 
-export default App;
+
